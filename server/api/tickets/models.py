311a4d7616eafff99529dad35527aec44ad3c9dc
Eric Phillips <eric@nekvtcode.com>
2020-06-08 05:17:50 -0400

    Update Ticket model

--- server/api/tickets/models.py
+++ server/api/tickets/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Ticket(models.Model):
     completed = models.BooleanField(default=False)
-    title = models.CharField(max_length=140)
-    description = models.TextField()
-
+    name = models.CharField(max_length=60)
+    title = models.CharField(max_length=120)
+    description = models.TextField(max_length=240, blank=True)
+    reply_to = models.EmailField(blank=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    completed_at = models.DateTimeField(null=True)
     def __str__(self):
         return self.title
